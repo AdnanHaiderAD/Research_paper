e45e17e5b5574e152e38cbb0ad1b624a0c95fc59
Mustafa Haider <s0840844@sms.ed.c.uk>
2014-02-26 22:39:26 +0000

    TIDIGITS data extraction successfully completed

--- dataset/read_speechSignals/convertToMFCC.m
+++ dataset/read_speechSignals/convertToMFCC.m
@@ -5,7 +5,7 @@ numberofSamples =length(RawData);
 MFCCdata =cell(numberofSamples,1);
 
 for i=1: numberofSamples
-    MFCCdata{i} = melfcc(RawData{i});
+    MFCCdata{i} = melfcc(RawData{i},10000);
 end
 save('MFCCTIDIGITS.mat','MFCCdata');
 end
