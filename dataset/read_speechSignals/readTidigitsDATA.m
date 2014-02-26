0f251d2768ab920468b90329c25c2bbb21980b7a
Mustafa Haider <s0840844@sms.ed.c.uk>
2014-02-26 22:01:23 +0000

    writing code to extract data is complete

--- dataset/read_speechSignals/readTidigitsDATA.m
+++ dataset/read_speechSignals/readTidigitsDATA.m
@@ -10,17 +10,24 @@ genders = strsplit(genderDir,' ');
 
 for i=1:length(genders)
     pathtoSpecificGender = strcat(pathname, '/', genders(i));
-    
+    if iscellstr(pathtoSpecificGender)
+        pathtoSpecificGender=pathtoSpecificGender{1};
+    end
     %% setup path to each individual speaker files
     speakerDir = ls (pathtoSpecificGender);
     listofSpeakers = strsplit(speakerDir,' ');
-    for j= 1:listofSpeakers
+    for j= 1:length(listofSpeakers)
         pathtoSpecificSpeaker = strcat(pathtoSpecificGender, '/', listofSpeakers(j));
+        if iscellstr(pathtoSpecificSpeaker)
+            pathtoSpecificSpeaker=pathtoSpecificSpeaker{1};
+        end
         data ={'1','2','3','4','5','6','7','8','9'};
         for k=1:length(data)
            %% setup data path
            datapath = strcat(pathtoSpecificSpeaker, '/', data(k), 'a.wav');
-           
+           if iscellstr(datapath)
+               datapath=datapath{1};
+           end
            %%read data and conduct preprocessing : resampling followed by silence removal
            speechdata= readnist(datapath);
            DATA{count}= filterSignal(speechdata);
