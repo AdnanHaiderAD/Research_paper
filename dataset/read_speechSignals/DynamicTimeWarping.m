8abb75abc5b774cfd96dccb9d23e26f170642ef1
Mustafa Haider <s0840844@sms.ed.c.uk>
2014-02-26 23:31:02 +0000

    TIDIGITS data extraction successfully completed

--- dataset/read_speechSignals/DynamicTimeWarping.m
+++ dataset/read_speechSignals/DynamicTimeWarping.m
@@ -1,21 +1,22 @@
-function optimumCost = DynamicTimeWarping(vargin)
+function optimumCost = DynamicTimeWarping(varargin)
 %%The DTW algorithm using a symmetric weighting function t ensure
 %%D(A,B)=D(B,A)
-
-if length(vargin)<2
+varargin
+if length(varargin)<2
     error('input:arg','The DTW arguments needs to be two patterns')
 end
 
 %%window parameter locality constaint
-windowSize =0;
-if length(vargin)==3
-    windowSize =vargin(3);
+window =0;
+if length(varargin)==3
+    window =varargin{3};
 end
 
 %% input patterns
-patternA =vargin(1)
-patternB = vargin(2);
-window = max(ceil(windowSize*(max(length(patternA),length(patternB)))),abs(length(patternA)-length(patternB)));
+patternA =varargin{1};
+patternB = varargin{2};
+
+windowSize = max(ceil(window*(max(length(patternA),length(patternB)))),abs(length(patternA)-length(patternB)));
 
 %% initializing the DTW cost matrix
 DTW = zeros(length(patternA)+1,length(patternB)+1);
@@ -30,8 +31,11 @@ end
 patternA = [zeros(length(patternA(:,1)),1) patternA];
 patternB = [zeros(length(patternA(:,1)),1) patternB];
 
-for i=2:length(pattenA)
-    for j=max(2,i-window):min(length(patternB),i+window)
+for i=2:length(patternA)
+    for j=2:length(patternB)
+        if (length(varargin)==3 && abs(i-j)>windowSize)
+            continue;
+        end
         cost = sum((patternA(:,i) -patternB(:,j)).^2);
         DTW(i,j)  = min([(DTW(i,j-1)+cost) (DTW(i-1,j-1)+2*cost) (DTW(i-1,j)+cost)]);
     end
