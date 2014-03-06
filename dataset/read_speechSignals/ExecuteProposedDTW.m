8351829326a0b993e978e92735960159a6cb8995
Mustafa Haider <s0840844@sms.ed.c.uk>
2014-03-06 11:52:43 +0000

    Running scripts again

--- dataset/read_speechSignals/ExecuteProposedDTW.m
+++ dataset/read_speechSignals/ExecuteProposedDTW.m
@@ -6,48 +6,27 @@ function ExecuteProposedDTW(MFCCDATA,SpeakersDigitsIndex, windowSize)
 MFCCDATA=PartitionMFCC(MFCCDATA,windowSize);
 
 tic;
-costMatrix=zeros(length(MFCCDATA),length(MFCCDATA));
-output= zeros(length(MFCCDATA),3);
+%%number of comparisons: N choose 2
+numberofComparisons= factorial(length(MFCCDATA))/(factorial(2)*factorial(length(MFCCDATA)-2));
+output= zeros(numberofComparisons,3);
 count=1;
-
-for i =1 : length(MFCCDATA)
+for i =1 :length(MFCCDATA)
     currentPattern =MFCCDATA{i};
     metaData =SpeakersDigitsIndex{i};
     speakerid =metaData{1};
     digitclass =metaData{2};
-    
-    min_cost=Inf;
-    id_closestspeaker='';
-    id_closestClass='';
-    for j=1:length(MFCCDATA)
-        if(i==j)
-            continue;
-        end
+    for j=i+1:length(MFCCDATA)
         pattern=MFCCDATA{j};
         meta =SpeakersDigitsIndex{j};
         spid =meta{1};
         dclass=meta{2};
-        
-        if (j<i)
-            cost =costMatrix(j,i);
-        else
-            cost= ProposedDynamicTimeWarping(currentPattern,pattern,windowSize);
-            costMatrix(i,j)=cost;
-        end
-        
-        if (cost <min_cost)
-             min_cost=cost;
-             id_closestspeaker=spid;
-             id_closestClass=dclass;
-        end
-    end
-    
-    output(i,1)=min_cost;
-    output(i,2)= strcmp(speakerid,id_closestspeaker);
-    output(i,3) =strcmp(digitclass,id_closestClass);
+    end    
+    output(count,1)=ProposedDynamicTimeWarping(currentPattern,pattern,windowSize);
+    output(count,2)= strcmp(speakerid,spid);
+    output(count,3)=strcmp(digitclass,dclass);
     count=count+1;
-    
 end
+
 time=toc;
 filename=['RDTW_w' num2str(windowSize) '.mat'];
 save (filename,'output','time');
