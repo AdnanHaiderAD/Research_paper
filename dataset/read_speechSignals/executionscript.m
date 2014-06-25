972b744fc4006ecb016d041fe0e83eb688586b2b
Mustafa Haider <s0840844@sms.ed.c.uk>
2014-06-25 22:39:41 +0100

    cleaned up code

--- dataset/read_speechSignals/executionscript.m
+++
@@ -1,5 +0,0 @@
-load ../data/SwitchBoard/MFCCSwitchBoard.mat
-load ../data/SwitchBoard/metadataSwitchboard.mat
-ExecuteDTW(MFCCdata,metadata,0.1,378,5206,17920);
-ExecuteDTW(MFCCdata,metadata,0.2);
-ExecuteDTW(MFCCdata,metadata,0.3);
--- dataset/read_speechSignals/executionscript2.m
+++
@@ -1,5 +0,0 @@
-load ../data/SwitchBoard/MFCCSwitchBoard.mat
-load ../data/SwitchBoard/metadataSwitchboard.mat
-ExecuteProposedDTW(MFCCdata,metadata,2,410,5030,17869);
-ExecuteProposedDTW(MFCCdata,metadata,3);
-ExecuteProposedDTW(MFCCdata,metadata,5);
