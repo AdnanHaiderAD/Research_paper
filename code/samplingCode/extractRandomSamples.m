6cce6973900693790d8ae7e8d1c98abc1ce79a95
Mustafa Haider <s0840844@sms.ed.c.uk>
2014-06-27 16:03:52 +0100

    ready to run test on Switchboard small sample set

--- code/samplingCode/extractRandomSamples.m
+++ code/samplingCode/extractRandomSamples.m
@@ -3,20 +3,26 @@
 load ../../dataset/data/SwitchBoard/MFCCSwitchBoard.mat
 load ../../dataset/data/SwitchBoard/metadataSwitchboard.mat
 
-MFCC_Samples =   cell(5000,1);
-metadata_sampled =  cell(5000,2);
-metadata2_sampled = cell(5000,3);
-for i=1:5000
+MFCC_Samples =   cell(900,1);
+metadata_sampled =  cell(1000,2);
+metadata2_sampled = cell(1000,3);
+sample_nums =zeros(1000,1);
+rand('state',sum(100*clock));
+for i=1:1000
     sample_Number = round(rand *11024);
+    while (sum(sample_Number==sample_nums)>0)
+          sample_Number = round(rand *11024);
+    end
+    sample_nums(i)=sample_Number;
     
     MFCC_Samples{i}= MFCCdata{sample_Number};
     
-    metadata_sampled{i,1} = metadata{i,1};
-    metadata_sampled{i,2} = metadata{i,2};
+    metadata_sampled{i,1} = metadata{sample_Number,1};
+    metadata_sampled{i,2} = metadata{sample_Number,2};
     
-    metadata2_sampled{i,1} = metadata2{i,1};
-    metadata2_sampled{i,2} = metadata2{i,2};
-    metadata2_sampled{i,3} = metadata2{i,3};
+    metadata2_sampled{i,1} = metadata2{sample_Number,1};
+    metadata2_sampled{i,2} = metadata2{sample_Number,2};
+    metadata2_sampled{i,3} = metadata2{sample_Number,3};
     
 end
 save('metadataOfSampleddata.mat','metadata_sampled','metadata2_sampled');
